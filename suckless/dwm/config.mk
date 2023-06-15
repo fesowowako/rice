c318dce74ed3185d863a52374674b37f682d93fa
fesowowako <118536485+fesowowako@users.noreply.github.com>
2023-06-15 10:06:18 +0300

    Update config.mk
    Signed-off-by: fesowowako <118536485+fesowowako@users.noreply.github.com>

--- suckless/dwm/config.mk
+++ suckless/dwm/config.mk
@@ -26,7 +26,7 @@ INCS = -I${X11INC} -I${FREETYPEINC}
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 
 # flags
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} -D_FORTIFY_SOURCE=1 -s -march=native -O2 \
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} -D_FORTIFY_SOURCE=1 -s -march=native -O2 \
 					 -ftree-vectorize -fno-semantic-interposition -pipe
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations ${INCS} ${CPPFLAGS}
